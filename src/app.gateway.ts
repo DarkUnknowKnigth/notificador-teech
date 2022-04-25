c1ad3923527386bdce105aeb5e4b475290dc49d1
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2022-04-25 15:47:38 -0500

    notificaciones masivas

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -4,10 +4,16 @@ import {
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  WsResponse,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-
+interface Notification {
+  segmento?: string;
+  titulo: string;
+  contenido: string;
+  url: string;
+  boletin: any;
+  publicacion: any;
+}
 @WebSocketGateway({
   cors: {
     origin: '*',
@@ -16,18 +22,17 @@ import { Server, Socket } from 'socket.io';
 export class AppGateway implements OnGatewayInit {
   @WebSocketServer() wss: Server;
   private logger: Logger = new Logger('WS-notificador');
-  afterInit(server: any) {
+  afterInit() {
     this.logger.log('Iniciado');
   }
-  @SubscribeMessage('notificacion')
-  notification(socket: Socket, data: any): WsResponse<any> {
-    return { event: 'notificacion', data };
+  @SubscribeMessage('notificador')
+  notification(socket: Socket, data: Notification): void {
+    this.logger.log('notificacion enviada');
+    this.logger.log(data);
+    this.wss.emit('notificacion', data);
   }
-  @SubscribeMessage('mensaje')
-  message(socket: Socket, data: any): WsResponse<any> {
-    return { event: 'mensaje', data };
-  }
-  sendNotification(data: any) {
-    this.wss.emit(data);
+  @SubscribeMessage('mensajero')
+  message(socket: Socket, data: Notification): void {
+    this.wss.emit('mensaje' + data.segmento, data);
   }
 }
